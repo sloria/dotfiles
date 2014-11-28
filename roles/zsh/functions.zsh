7e964df69fb6d5fe6c6b41ddaf7f8766d62de72f
Steven Loria <sloria1@gmail.com>
2014-11-28 10:51:16 -0600

    Add scratch command

--- roles/zsh/functions.zsh
+++ roles/zsh/functions.zsh
@@ -24,3 +24,14 @@ extract () {
         echo "'$1' is not a valid file"
     fi
 }
+
+# credit: http://ku1ik.com/2012/05/04/scratch-dir.html
+# Creates a new scratch directory and cds to it
+function scratch {
+  cur_dir="$HOME/scratch"
+  new_dir="$HOME/tmp/scratch-`date +'%s'`"
+  mkdir -p $new_dir
+  ln -nfs $new_dir $cur_dir
+  cd $cur_dir
+  echo "New scratch dir ready for grinding ;>"
+}
