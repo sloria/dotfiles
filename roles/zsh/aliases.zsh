31da6a4a1780367b302e95e848a403c724add8e2
Steven Loria <sloria1@gmail.com>
2015-02-01 13:08:27 -0500

    Add update alias; minor vimrc change

--- roles/zsh/aliases.zsh
+++ roles/zsh/aliases.zsh
@@ -1,6 +1,9 @@
 # Pipe my public key to my clipboard.
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 
+# Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update --system; sudo gem update'
+
 alias cl="clear"
 alias c="clear"
 alias pg='ps -ef | grep'
