635192fc18f4a85d545ad582a66d4d2267c3b296
Steven Loria <sloria1@gmail.com>
2015-03-28 13:45:46 -0400

    Rename env.zsh -> _env.zsh so it executes early

--- roles/zsh/aliases.zsh
+++ roles/zsh/aliases.zsh
@@ -2,7 +2,7 @@
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 
 # Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
-alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update --system; sudo gem update'
+alias update!='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update --system; sudo gem update'
 
 alias cl="clear"
 alias c="clear"
@@ -10,6 +10,5 @@ alias pg='ps -ef | grep'
 alias lj='jobs'
 
 alias reload!='. ~/.zshrc'
-alias zshconfig="exec $EDITOR ~/.zshrc"
-alias vimrc="exec $EDITOR ~/.vimrc"
 alias vi="vim"
+alias v="vim"
