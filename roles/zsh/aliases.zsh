1ac5eecdb20074e84e4643f755e4422330e70001
Steven Loria <sloria1@gmail.com>
2015-03-30 22:58:03 -0400

    Use highlight for syntax-highlighting text from clipboard

--- roles/zsh/aliases.zsh
+++ roles/zsh/aliases.zsh
@@ -12,3 +12,14 @@ alias lj='jobs'
 alias reload!='. ~/.zshrc'
 alias vi="vim"
 alias v="vim"
+# Syntax-highlight text from clipboard
+# Requires highlight: brew install highlight
+# Usage: hit js
+function hit() {
+  if [ -z "$2" ]
+    then src="pbpaste"
+  else
+    src="cat $2"
+  fi
+  $src | highlight -O rtf --syntax $1 --font 'Ubuntu Mono' --style solarized-light --font-size 24 | pbcopy
+}
