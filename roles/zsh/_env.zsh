7eb211ab3aa2b3a77e638275e746bb69dfc57386
Steven Loria <sloria1@gmail.com>
2015-09-06 22:00:11 -0400

    Add PED_EDITOR

--- roles/zsh/_env.zsh
+++ roles/zsh/_env.zsh
@@ -1,5 +1,7 @@
 export EDITOR='mvim'
 export VISUAL='mvim'
+# For ped: https://github.com/sloria/ped
+export PED_EDITOR='vim'
 export TERM=xterm-256color
 
 # Used by karma: Since we use cask to install browsers, the bin files will be located
