9f0129ffabde4c46997d963b0ca4562dfdd3aade
Steven Loria <sloria1@gmail.com>
2015-09-26 10:26:37 -0400

    Update vimrc and gitconfig; move hit function

--- roles/zsh/functions.zsh
+++ roles/zsh/functions.zsh
@@ -56,3 +56,16 @@ fancy-ctrl-z () {
 }
 zle -N fancy-ctrl-z
 bindkey '^Z' fancy-ctrl-z
+
+
+# Syntax-highlight text from clipboard
+# Requires highlight: brew install highlight
+# Usage: hit js
+function hit() {
+  if [ -z "$2" ]
+    then src="pbpaste"
+  else
+    src="cat $2"
+  fi
+  $src | highlight -O rtf --syntax $1 --font 'Ubuntu Mono' --style solarized-light --font-size 24 | pbcopy
+}
