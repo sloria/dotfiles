bbdb62ab0957131bef3278fe9ae334094108badc
Steven Loria <sloria1@gmail.com>
2015-10-17 10:08:06 -0400

    Move pyclean

--- roles/python/aliases.zsh
+++ roles/python/aliases.zsh
@@ -17,10 +17,3 @@ alias lsenv="conda info -e"
 alias wo="source activate"
 alias de="source deactivate"
 
-# Remove python compiled byte-code in either current directory or in a
-# list of specified directories
-function pyclean() {
-    ZSH_PYCLEAN_PLACES=${*:-'.'}
-    find ${ZSH_PYCLEAN_PLACES} -type f -name "*.py[co]" -delete
-    find ${ZSH_PYCLEAN_PLACES} -type d -name "__pycache__" -delete
-}
--- roles/python/functions.zsh
+++ roles/python/functions.zsh
@@ -17,3 +17,11 @@ coff () {
     path=($_path)
     unset CONDA_DEFAULT_ENV
 }
+
+# Remove python compiled byte-code in either current directory or in a
+# list of specified directories
+function pyclean() {
+    ZSH_PYCLEAN_PLACES=${*:-'.'}
+    find ${ZSH_PYCLEAN_PLACES} -type f -name "*.py[co]" -delete
+    find ${ZSH_PYCLEAN_PLACES} -type d -name "__pycache__" -delete
+}
