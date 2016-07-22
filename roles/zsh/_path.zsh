0ccb9c5752bd5f271f5b62c16825f49c60594b20
Steven Loria <sloria1@gmail.com>
2016-07-21 21:58:25 -0500

    Install Postgres.app with cask

--- roles/zsh/_path.zsh
+++ roles/zsh/_path.zsh
@@ -1,3 +1,5 @@
 # Customize to your needs...
 # Make sure to APPEND to the path, so that homebrewed binaries, e.g. /usr/local/bin are found first
 export PATH=${PATH}:$ZSH/bin
+# For Postgres.app
+export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
