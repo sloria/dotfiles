713f45058d2a347cf0e51bfd0b7720776ed63723
Steven Loria <sloria1@gmail.com>
2016-10-29 13:27:26 -0400

    Update set-defaults to not break key repeat

--- roles/osx/files/set-defaults.sh
+++ roles/osx/files/set-defaults.sh
@@ -7,14 +7,8 @@
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
-# Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
-
 # Use AirDrop over every interface. srsly this should be a default.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
 # Show the ~/Library folder.
 chflags nohidden ~/Library
-
-# Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 0
