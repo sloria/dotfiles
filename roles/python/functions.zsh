0efacbb4771fcf1a7c6755cb9c6afc21e186c2ad
Steven Loria <sloria1@gmail.com>
2016-11-19 14:35:42 -0500

    Add rmpdb; update homebrew packages

--- roles/python/functions.zsh
+++ roles/python/functions.zsh
@@ -25,3 +25,10 @@ function pyclean() {
     find ${ZSH_PYCLEAN_PLACES} -type f -name "*.py[co]" -delete
     find ${ZSH_PYCLEAN_PLACES} -type d -name "__pycache__" -delete
 }
+
+
+# Clear all ipdb statements
+function rmpdb() {
+    git ls-files -oc --exclude-standard "*.py" | cat | xargs sed -i '' '/import ipdb;/d'
+    echo "Cleared breakpoints"
+}
