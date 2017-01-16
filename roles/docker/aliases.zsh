b242b0fa4570316bc9fa4653fe48dc7fea931db2
Steven Loria <sloria1@gmail.com>
2017-01-16 11:41:38 -0500

    More docker aliases

--- roles/docker/aliases.zsh
+++ roles/docker/aliases.zsh
@@ -1,3 +1,7 @@
+alias dk="docker "
 alias dc="docker-compose"
+alias dcup="docker-compose up -d "
+alias dcrun="docker-compose run --rm "
+# Stop and recreate a container
 alias dcreup="docker-compose up -d --force-recreate --no-deps "
 alias dclog="docker-compose logs -f --tail 100 "
