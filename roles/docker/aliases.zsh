94c5d55d05138b306b6f4826184ae7a4ecc7aabb
Steven Loria <sloria1@gmail.com>
2017-02-20 19:59:19 -0500

    Add a couple docker-sync aliases

--- roles/docker/aliases.zsh
+++ roles/docker/aliases.zsh
@@ -1,12 +1,17 @@
 alias dk="docker "
 alias dc="docker-compose"
+
 alias dcup="docker-compose up -d "
 alias dcrun="docker-compose run --rm "
 # Stop and recreate a container
 alias dcreup="docker-compose up -d --force-recreate --no-deps "
 alias dclog="docker-compose logs -f --tail 100 "
 alias dcl="docker-compose logs -f --tail 100 "
+
+alias ds="docker-sync"
 alias dss="docker-sync start"
+alias dsx="docker-sync stop"
+
 alias dcbomb!="docker-compose down -v"
 
 function dcbash {
