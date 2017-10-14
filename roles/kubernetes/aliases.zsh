af8d827ed14ac105475fcaec30f61a5e48eff439
Steven Loria <sloria1@gmail.com>
2017-10-14 08:48:12 -0500

    Add kubelogs function

--- roles/kubernetes/aliases.zsh
+++ roles/kubernetes/aliases.zsh
@@ -8,8 +8,8 @@ function kubepods() {
   fi
 }
 
-# Usage: kubebash stage-3-osf web uwsgi
-#        kubebash stage-3-osf postgres
+# Usage: kubebash stage-3 web uwsgi
+#        kubebash stage-3 postgres
 function kubebash() {
   namespace=$1
   namere=$2
@@ -21,5 +21,21 @@ function kubebash() {
   else
     echo 'Entering the default container on' $name 'in namespace' $namespace
     kubectl --namespace $namespace exec -it $name bash
+  fi
+}
+
+# Usage: kubelogs stage-3 web uwsgi
+#        kubelogs stage-3 postgres
+function kubelogs() {
+  namespace=$1
+  namere=$2
+  name=$(kubectl get pods -n $namespace | grep $namere | head -1 | grep -o '^\S\+')
+  if [[ $# -gt 2 ]]; then
+    container=$3
+    echo 'Tailing logs from the' $container 'container on' $name 'in namespace' $namespace
+    kubectl --namespace $namespace logs -f --tail 100 $name -c $container
+  else
+    echo 'Tailing logs from the default container on' $name 'in namespace' $namespace
+    kubectl --namespace $namespace logs -f --tail 100 $name
   fi
 }
