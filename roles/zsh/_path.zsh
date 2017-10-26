21c7adb189cf18f84b277e2e1c260313243e3315
Steven Loria <sloria1@gmail.com>
2017-10-25 22:09:33 -0400

    Clean up junk

--- roles/zsh/_path.zsh
+++ roles/zsh/_path.zsh
@@ -1,5 +1,3 @@
 # Customize to your needs...
 # Make sure to APPEND to the path, so that homebrewed binaries, e.g. /usr/local/bin are found first
 export PATH=${PATH}:$ZSH/bin
-# For Postgres.app
-export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
