2054362fd5dcb72d6d1696331924d92579da2834
Steven Loria <sloria1@gmail.com>
2018-01-23 19:01:57 +0800

    More defaults

--- roles/osx/files/set-defaults.sh
+++ roles/osx/files/set-defaults.sh
@@ -1,14 +1,24 @@
-# Sets reasonable OS X defaults.
-#
-# Or, in other words, set shit how I like in OS X.
-#
-# The original idea (and a couple settings) were grabbed from:
-#   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
-#
-# Run ./set-defaults.sh and you'll be good to go.
+#!/bin/sh
 
-# Use AirDrop over every interface. srsly this should be a default.
+# set reasonable macOS defaults
+# inspired by : https://github.com/mathiasbynens/dotfiles
+# more can be found here : https://gist.github.com/brandonb927/3195465
+
+if [ "$(uname -s)" != "Darwin" ]; then
+	exit 0
+fi
+
+set +e
+
+echo "  › Use AirDrop over every interface. srsly this should be a default."
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
-# Show the ~/Library folder.
+echo "  › show the ~/Library folder"
 chflags nohidden ~/Library
+
+echo "  › disable smart quotes and smart dashes as they're annoying when typing code"
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+echo "  › show path bar"
+defaults write com.apple.finder ShowPathbar -bool true
