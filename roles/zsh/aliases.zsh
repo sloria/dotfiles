63c71afd73adaa0888154a5fbaee2f9ecd3c4958
Steven Loria <sloria1@gmail.com>
2018-02-19 13:21:51 -0500

    Remove dog, since I'm using ccat now

--- roles/zsh/aliases.zsh
+++ roles/zsh/aliases.zsh
@@ -1,9 +1,6 @@
 # Pipe my public key to my clipboard.
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 
-# Syntax-highlighted cat (requires python-pygments)
-alias dog="pygmentize -g"
-
 alias cl="clear"
 alias c="clear"
 alias pg='ps -ef | grep'
