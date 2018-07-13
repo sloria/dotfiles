9ba3a0b19453a367ed2626a5823511520a031f31
Steven Loria <sloria1@gmail.com>
2018-07-13 09:37:12 -0400

    Lazily-load kubectl completion to speed up start-up time

--- roles/kubernetes/completion.zsh
+++ roles/kubernetes/completion.zsh
@@ -1,1 +1,10 @@
-source <(kubectl completion zsh)
+# Lazily load completion to speed up zsh startup time
+# https://stackoverflow.com/a/48202907/1157536
+kubectl () {
+    if [[ -z $KUBECTL_COMPLETE ]]
+    then
+        source <($commands[kubectl] completion zsh)
+        KUBECTL_COMPLETE=1
+    fi
+    $commands[kubectl] $*
+}
