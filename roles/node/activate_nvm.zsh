67e0465edf5984c4f4b78711ab32bbac525c52b3
Steven Loria <sloria1@gmail.com>
2018-07-16 19:37:23 -0400

    Use group_lazy_load for nvm/npm/node
    So that node can be run without having to run nvm first

--- roles/node/activate_nvm.zsh
+++ roles/node/activate_nvm.zsh
@@ -1,12 +1,4 @@
 # Lazily load nvm so that it doesn't slow down shell startup time
-# https://github.com/creationix/nvm/issues/782#issuecomment-299669827
+# https://github.com/creationix/nvm/issues/782
 export NVM_DIR="$HOME/.nvm"
-function nvm {
-  # This loads nvm
-  if [ -s "$NVM_DIR/nvm.sh" ]; then
-    . "$NVM_DIR/nvm.sh"
-    nvm use system
-    nvm $@
-  fi
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-}
+group_lazy_load $HOME/.nvm/nvm.sh nvm node npm npx
