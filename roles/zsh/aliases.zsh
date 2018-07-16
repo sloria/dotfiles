2c3c0d2faa22547a5c5382c16f5c8aca6c5888d6
Steven Loria <sloria1@gmail.com>
2018-07-16 19:24:33 -0400

    Add pkill and fkill aliases

--- roles/zsh/aliases.zsh
+++ roles/zsh/aliases.zsh
@@ -4,6 +4,8 @@ alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pa
 alias cl="clear"
 alias c="clear"
 alias pg='ps -ef | grep'
+alias pkill!="pkill -9 -f "
+alias fkill="fkill -v"
 alias lj='jobs'
 alias dil='doitlive'
 alias dilp='doitlive play'
