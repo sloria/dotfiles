904a893cba1707eb5d819fdcad43e52729a950f3
Steven Loria <sloria1@gmail.com>
2018-07-17 19:00:10 -0400

    Also load nvm when running yarn

--- roles/node/activate_nvm.zsh
+++ roles/node/activate_nvm.zsh
@@ -1,4 +1,4 @@
 # Lazily load nvm so that it doesn't slow down shell startup time
 # https://github.com/creationix/nvm/issues/782
 export NVM_DIR="$HOME/.nvm"
-group_lazy_load $HOME/.nvm/nvm.sh nvm node npm npx
+group_lazy_load $HOME/.nvm/nvm.sh nvm node npm npx yarn
