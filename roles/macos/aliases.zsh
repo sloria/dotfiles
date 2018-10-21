c3ed77bac2d7518023af5b8dfabb27d4b828fcca
Steven Loria <sloria1@gmail.com>
2018-10-21 19:51:23 -0400

    Use noti instead of `yell`

--- roles/macos/aliases.zsh
+++ roles/macos/aliases.zsh
@@ -7,8 +7,6 @@ function sha256() {
 
 # Display a notification with a given message
 alias growl="terminal-notifier -message"
-# Useful for notifying when a long script finishes
-alias yell="terminal-notifier -title WOOOO -message OOOO!!!"
 
 # Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
 alias update!='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update --system; sudo gem update'
