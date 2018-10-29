53d295760b3b50a1d259c54bcb76b5f6e824db51
Steven Loria <sloria1@gmail.com>
2018-10-29 09:48:11 -0400

    Fix kubectl completion

--- roles/kubernetes/completion.zsh
+++ roles/kubernetes/completion.zsh
@@ -1,10 +1,10 @@
 # Lazily load completion to speed up zsh startup time
 # https://stackoverflow.com/a/48202907/1157536
 kubectl () {
+    command kubectl $*
     if [[ -z $KUBECTL_COMPLETE ]]
     then
-        source <($commands[kubectl] completion zsh)
-        KUBECTL_COMPLETE=1
+        source <(command kubectl completion zsh)
+        KUBECTL_COMPLETE=1 
     fi
-    $commands[kubectl] $*
 }
