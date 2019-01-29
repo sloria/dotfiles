ecfe01d690eb887b9caf1a48f8d277629690a724
Steven Loria <sloria1@gmail.com>
2019-01-29 11:46:20 -0500

    Add FAM_SOURCE and FAM_EDITOR envvars

--- roles/zsh/_env.zsh
+++ roles/zsh/_env.zsh
@@ -1,11 +1,13 @@
+export TERM=xterm-256color
 export EDITOR='mvim'
 export VISUAL='mvim'
-# For ped: https://github.com/sloria/ped
+export GPG_TTY=$(tty)
+
+# Settings for various CLIs I use
 export PED_EDITOR='vim'
 export KONCH_EDITOR='vim'
-export TERM=xterm-256color
-
-export GPG_TTY=$(tty)
+export FAM_SOURCE="$HOME/Dropbox/fam"
+export FAM_EDITOR="vim"
 
 # Used by karma: Since we use cask to install browsers, the bin files will be located
 # in ~/Applications instead of /Applications (Mac-specific)
