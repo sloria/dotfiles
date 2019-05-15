4cff10c0c9cb6c4fae19d575d7ec61740ecaee62
Steven Loria <sloria1@gmail.com>
2019-05-15 16:46:49 -0400

    remove junk

--- roles/python/functions.zsh
+++ roles/python/functions.zsh
@@ -6,26 +6,12 @@ function pyclean() {
     find ${ZSH_PYCLEAN_PLACES} -type d -name "__pycache__" -delete
 }
 
-function build-readme() {
-    if [[ -e README.html ]]; then
-        rm README.html
-    fi
-    rst2html.py README.rst > README.html
-    open README.html
-}
-
 # Build docs in watch mode.
 # requires sphinx-autobuild (pip install sphinx-autobuild)
 function sphinxwatch() {
     sphinx-autobuild --open-browser docs docs/_build
 }
 
-
-# Clear all ipdb statements
-function rmpdb() {
-    git ls-files -oc --exclude-standard "*.py" | cat | xargs sed -i '' '/import ipdb;/d'
-    echo "Cleared breakpoints"
-}
 
 # Generate fake data, e.g. `fake name`, `fake url`, `fake email`
 # Requires faker to be installed (pip install faker)
