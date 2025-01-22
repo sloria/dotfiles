ff10bb1bcc4ca1e5c234f5903daef0d100557864
Steven Loria <sloria1@gmail.com>
2025-01-22 12:10:27 -0500

    Update nvm install script and use it's built-in lazy-loading

--- roles/node/activate_nvm.zsh
+++ roles/node/activate_nvm.zsh
@@ -1,4 +1,5 @@
-# Lazily load nvm so that it doesn't slow down shell startup time
-# https://github.com/creationix/nvm/issues/782
-export NVM_DIR="$HOME/.nvm"
-group_lazy_load $HOME/.nvm/nvm.sh nvm node npm npx yarn
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+
+# --no-use lazy-loads nvm until you run nvm use: https://github.com/nvm-sh/nvm?tab=readme-ov-file#additional-notes
+# This speeds up shell startup time significantly
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use # This loads nvm
