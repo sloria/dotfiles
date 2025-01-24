d5444b8ed5ec271d1f1bfd0bef753297ba0b9ef0
Steven Loria <sloria1@gmail.com>
2025-01-24 14:05:01 -0500

    Disable zsh's auto-cd behavior

--- roles/zsh/config.zsh
+++ roles/zsh/config.zsh
@@ -2,3 +2,6 @@
 DISABLE_CORRECTION="true"
 unsetopt correct
 unsetopt correct_all
+# Don't auto-cd into directories without typing cd
+# because can annoyingly put you into source directories for CLI projects
+unsetopt AUTO_CD
