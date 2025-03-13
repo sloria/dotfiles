a643065112c4eaf58c2ce3411367290733a4705f
Steven Loria <sloria1@gmail.com>
2025-03-12 22:01:52 -0400

    Update jrnl and fam config

--- roles/zsh/_env.zsh
+++ roles/zsh/_env.zsh
@@ -7,5 +7,5 @@ export LC_ALL=en_US.UTF-8
 # Settings for various CLIs I use
 export PED_EDITOR='vim'
 export KONCH_EDITOR='vim'
-export FAM_SOURCE="$HOME/Dropbox/fam"
+export FAM_SOURCE="$HOME/iCloud/fam"
 export FAM_EDITOR="vim"
