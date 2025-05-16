ce5fd8b725e5ff101d0bc1798ef044729fd36e73
Steven Loria <sloria1@gmail.com>
2025-05-15 21:01:12 -0400

    Update mise config; pyserv->serve

--- roles/python/aliases.zsh
+++ roles/python/aliases.zsh
@@ -1,6 +1,6 @@
 alias py="python"
 alias ipy="ipython"
-alias pyserv="uvx --python 3.13 python -m http.server"
+alias serve="uvx --python 3.13 python -m http.server"
 alias pt="pytest"
 
 alias pipgrep="pip freeze | grep -i "
