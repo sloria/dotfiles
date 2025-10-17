3a6fe9e8d354053c15d850a123d7a771e1a9b8f1
Steven Loria <sloria1@gmail.com>
2025-10-17 11:50:24 -0400

    Update docker aliases

--- roles/docker/aliases.zsh
+++ roles/docker/aliases.zsh
@@ -1,31 +1,31 @@
-alias d="docker "
-alias dlog="docker logs -f --tail 100 "
-alias dl="docker logs -f --tail 100 "
-alias ddf="docker system df"
-# Stop and remove a single container
-alias drm='docker rm -f '
+alias dlogs="docker logs -f --tail 100 "
 # Stop and remove all running containers
 alias drm!='docker rm -f $(docker ps -a -q)'
 
 alias dc="docker compose"
 alias dcup="docker compose up -d "
 alias dcrun="docker compose run --rm "
-function dcdebug {
-  docker compose kill $1
-  docker compose run --service-ports $1
-}
 # Stop and recreate a container
 alias dcre="docker compose up -d --force-recreate --no-deps "
+alias dclogs="docker compose logs -f --tail 100 "
 alias dcl="docker compose logs -f --tail 100 "
 
-alias dcbomb!="docker compose -f deploy/docker-compose.infra.yml down -v --remove-orphans >/dev/null 2>&1"
-
-# Open bash in a container. Pass docker-compose name
+# Open bash in a container. Pass compose name
 function dcbash {
   docker compose exec $1 /bin/bash
+}
+
+# Open sh in a container. Pass compose name
+function dcsh {
+  docker compose exec $1 /bin/sh
 }
 
 # Open bash in a container. Pass container name
 function dbash {
   docker exec -it $1 /bin/bash
+}
+
+# Open sh in a container. Pass container name
+function dsh {
+  docker exec -it $1 /bin/sh
 }
