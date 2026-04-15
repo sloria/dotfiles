d606f0b0b476f02c52e3df94cbf90861666bc296
Steve <git@stevenloria.com>
2026-04-15 10:17:35 -0400

    add option to allow SSH agent forwarding in VMs

--- roles/1password/env.zsh
+++ roles/1password/env.zsh
@@ -1,1 +1,1 @@
-export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+[[ "$OSTYPE" == darwin* ]] && export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
