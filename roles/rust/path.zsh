41fc9ba98ff981f3eed27e98e05d8315880a9cce
Steven Loria <git@stevenloria.com>
2026-07-13 14:17:47 -0400

    fix rust path

--- roles/rust/path.zsh
+++ roles/rust/path.zsh
@@ -1,1 +1,5 @@
-. "$HOME/.cargo/env"
+if [[ -s "$HOME/.cargo/env" ]]; then
+  . "$HOME/.cargo/env"
+elif [[ "$OSTYPE" == darwin* ]] && (( $+commands[brew] )); then
+  export PATH="$(brew --prefix rustup)/bin:$PATH"
+fi
